598520922dce99dc0bce075b44709290059db9a2
Rodriguez <elbacristinarodriguezperdomo@gmail.com>
2020-08-17 22:52:22 -0600

    Diseño

--- app/src/main/java/com/ugb/calculadora/MainActivity.java
+++ app/src/main/java/com/ugb/calculadora/MainActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String [][] moneda = {
+            {"Seleccione Conversor"},
+            {"Bitcoin","Colon salvadoreño", "Euro", },
+            {"", "San Jorge", "San Rafael"},
+            {"SRL","La Union","Anamoros","El Carmen"},
+            {""}
+    };
 
 
     @Override
@@ -18,7 +28,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TabHost tbhConversores = (TabHost)findViewById(R.id.tabconversores);
+        tbhConversores.setup();
 
+        tbhConversores.addTab(tbhConversores.newTabSpec("Direccion").setContent(R.id.tbsMasa).setIndicator("DIRECCION", null));
+        tbhConversores.addTab(tbhConversores.newTabSpec("Pais").setContent(R.id.tbsVolumen).setIndicator("PAIS", null));
+
+        final Spinner spnMun = (Spinner)findViewById(R.id.spnconv);
+
+            }
     }
 
-}
