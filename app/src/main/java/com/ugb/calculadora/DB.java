9562ea02a97e2ba06605a8e7fbd08408ae7d1a42
Josselyn Rodríguez <JosselynRodriguez2328@hotmail.com>
2020-09-23 12:21:21 -0600

    Inicio, agregar y modificar

---
+++ app/src/main/java/com/ugb/calculadora/DB.java
@@ -0,0 +1,20 @@
+package com.ugb.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+
+public class DB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d_b);
+    }
+    }
+
+
+
+
+
