fdafd52c166324a5dafaeda4130c507fd846e810
laura623 <laurriguerrero@gmail.com>
2020-10-09 01:43:06 -0600

    Inicio del Codigo

--- app/src/main/java/com/ugb/calculadora/MainActivity.java
+++ app/src/main/java/com/ugb/calculadora/MainActivity.java
@@ -2,15 +2,92 @@ package com.ugb.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-        public Button btnCalcular;
+        JSONArray datosJSON;
+        JSONObject jsonObject;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                obtenerProductos objObtenerProductos = new obtenerProductos();
+                objObtenerProductos.execute();
+        }
+        private class obtenerProductos extends AsyncTask<Void,Void, String> {
+                HttpURLConnection urlConnection;
+
+
+                @Override
+                protected String doInBackground(Void... voids) {
+                        StringBuilder result = new StringBuilder();
+                        try {
+                                URL url = new URL("Http://10.0.2.2:5984/db_agenda/_design/agenda/_view/mi-agenda");
+                                urlConnection = (HttpURLConnection) url.openConnection();
+                                urlConnection.setRequestMethod("GET");
+
+                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                                String linea;
+                                while ((linea = reader.readLine()) != null) {
+                                        result.append(linea);
+                                }
+                        } catch (Exception ex) {
+                                //
+                        }
+                        return result.toString();
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                        super.onPostExecute(s);
+                        try {
+                                jsonObject = new JSONObject(s);
+                                datosJSON = jsonObject.getJSONArray("rows");
+                                mostrarDatosProductos();
+                        } catch (Exception ex) {
+                                Toast.makeText(MainActivity.this, "Error la parsear los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                }
+        }
+        private void mostrarDatosProductos(){
+                ListView ltsAmigos = findViewById(R.id.ltsProductosCouchDB);
+                try {
+                        final ArrayList<String> arrayList = new ArrayList<>();
+                        final ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+                        ltsAmigos.setAdapter(stringArrayAdapter);
+
+                        for (int i = 0; i < datosJSON.length(); i++) {
+                                stringArrayAdapter.add(datosJSON.getJSONObject(i).getJSONObject("value").getString("nombre"));
+                        }
+                        stringArrayAdapter.notifyDataSetChanged();
+                        registerForContextMenu(ltsAmigos);
+                }catch (Exception ex){
+                        Toast.makeText(MainActivity.this, "Error al mostrar los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+                }
+        }
 
 }
