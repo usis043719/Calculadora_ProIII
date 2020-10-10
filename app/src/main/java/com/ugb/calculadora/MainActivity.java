96c6a3fa96a9701141590b4465f102ce706ad0fe
Rodriguez <elbacristinarodriguezperdomo@gmail.com>
2020-10-10 17:16:30 -0600

    primera parte del codigo

--- app/src/main/java/com/ugb/calculadora/MainActivity.java
+++ app/src/main/java/com/ugb/calculadora/MainActivity.java
@@ -2,6 +2,9 @@ package com.ugb.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -25,69 +28,66 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-        JSONArray datosJSON;
-        JSONObject jsonObject;
+    JSONArray datosJSON;
+    JSONObject JsonObject;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        obtenerDatostienda objObtenerproductos =new obtenerDatostienda();
+        objObtenerproductos.execute();
+    }
+    private class obtenerDatostienda extends AsyncTask<Void,Void, String>{
+        HttpURLConnection urLconection;
+        @Override
+        protected String doInBackground(Void... voids) {
+            StringBuilder result = new StringBuilder();
+            try {
+                URL url = new URL("Http://10.0.2.2:5984/db_tienda/_design/tienda/_view/mi-tienda");
+                urLconection = (HttpURLConnection) url.openConnection();//se conecta al servidor
+                urLconection.setRequestMethod("GET");
+
+                InputStream in = new BufferedInputStream(urLconection.getInputStream());//obtener datos
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String linea; //lear los datos
+                while ((linea = reader.readLine()) != null) {
+                    result.append(linea);
+                }
+            } catch (Exception ex) {
+                //
+            }
+            return result.toString();
+        }
 
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-
-                obtenerProductos objObtenerProductos = new obtenerProductos();
-                objObtenerProductos.execute();
+        protected void onPostExecute(String s) {//recibr los datos
+            super.onPostExecute(s);
+            try {
+                JsonObject = new JSONObject(s);
+                datosJSON = JsonObject.getJSONArray("rows");
+                mostrarDatosProductos();
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error la parsear los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+            }
         }
-        private class obtenerProductos extends AsyncTask<Void,Void, String> {
-                HttpURLConnection urlConnection;
+    }
+    private void mostrarDatosProductos(){
+        ListView ltsProductosCouchDB = findViewById(R.id.ltsProductosCouchDB);
+        try {
+            final ArrayList<String>arrayList = new ArrayList<>();
+            final ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+            ltsProductosCouchDB.setAdapter(stringArrayAdapter);
 
-
-                @Override
-                protected String doInBackground(Void... voids) {
-                        StringBuilder result = new StringBuilder();
-                        try {
-                                URL url = new URL("Http://10.0.2.2:5984/db_agenda/_design/agenda/_view/mi-agenda");
-                                urlConnection = (HttpURLConnection) url.openConnection();
-                                urlConnection.setRequestMethod("GET");
-
-                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                                String linea;
-                                while ((linea = reader.readLine()) != null) {
-                                        result.append(linea);
-                                }
-                        } catch (Exception ex) {
-                                //
-                        }
-                        return result.toString();
-                }
-
-                @Override
-                protected void onPostExecute(String s) {
-                        super.onPostExecute(s);
-                        try {
-                                jsonObject = new JSONObject(s);
-                                datosJSON = jsonObject.getJSONArray("rows");
-                                mostrarDatosProductos();
-                        } catch (Exception ex) {
-                                Toast.makeText(MainActivity.this, "Error la parsear los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                }
+            for (int i = 0; i < datosJSON.length(); i++) {
+                stringArrayAdapter.add(datosJSON.getJSONObject(i).getJSONObject("value").getString("Nombre"));
+            }
+            stringArrayAdapter.notifyDataSetChanged(); //reactualizar datos
+            registerForContextMenu(ltsProductosCouchDB);
+        }catch (Exception ex){
+            Toast.makeText(MainActivity.this, "Error al mostrar los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
         }
-        private void mostrarDatosProductos(){
-                ListView ltsAmigos = findViewById(R.id.ltsProductosCouchDB);
-                try {
-                        final ArrayList<String> arrayList = new ArrayList<>();
-                        final ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
-                        ltsAmigos.setAdapter(stringArrayAdapter);
-
-                        for (int i = 0; i < datosJSON.length(); i++) {
-                                stringArrayAdapter.add(datosJSON.getJSONObject(i).getJSONObject("value").getString("nombre"));
-                        }
-                        stringArrayAdapter.notifyDataSetChanged();
-                        registerForContextMenu(ltsAmigos);
-                }catch (Exception ex){
-                        Toast.makeText(MainActivity.this, "Error al mostrar los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-                }
-        }
+    }
 
 }
---
+++ app/src/main/java/com/ugb/calculadora/agregar_productos.java
@@ -0,0 +1,14 @@
+package com.ugb.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class agregar_productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_productos);
+    }
+}
