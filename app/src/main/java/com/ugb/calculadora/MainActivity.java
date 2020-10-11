27d1f99708d6f9ac9f4fd6cc50a9404ca9a2ad0b
Rodriguez <elbacristinarodriguezperdomo@gmail.com>
2020-10-11 13:43:25 -0600

    Arreglo del codigo

--- app/src/main/java/com/ugb/calculadora/MainActivity.java
+++ app/src/main/java/com/ugb/calculadora/MainActivity.java
@@ -39,6 +39,10 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     JSONArray datosJSON;
     JSONObject JsonObject;
+    Integer posicion;
+    ArrayList<String> arrayList =new ArrayList<String>();
+    ArrayList<String> copyStringArrayList = new ArrayList<String>();
+    ArrayAdapter<String> stringArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +51,80 @@ public class MainActivity extends AppCompatActivity {
 
         obtenerDatostienda objObtenerproductos =new obtenerDatostienda();
         objObtenerproductos.execute();
-    }
 
+        FloatingActionButton btnAgregarNuevoAmigos = findViewById(R.id.btnAgregarProductos);
+        btnAgregarNuevoAmigos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agregarNuevosProductos("nuevo", JsonObject);
+            }
+        });
+        buscarProductos();
+    }
+    void buscarProductos(){
+        final TextView tempVal = (TextView)findViewById(R.id.txtBuscarProducto);
+        tempVal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                arrayList.clear();
+                if( tempVal.getText().toString().trim().length()<1 ){//no hay texto para buscar
+                    arrayList.addAll(copyStringArrayList);
+                } else{//hacemos la busqueda
+                    for (String Producto : copyStringArrayList){
+                        if(Producto.toLowerCase().contains(tempVal.getText().toString().trim().toLowerCase())){
+                            arrayList.add(Producto);
+                        }
+                    }
+                }
+                stringArrayAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_principal, menu);
+        try {
+            AdapterView.AdapterContextMenuInfo adapterContextMenuInfo = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            posicion = adapterContextMenuInfo.position;
+            menu.setHeaderTitle(datosJSON.getJSONObject(posicion).getString("nombre"));
+        }catch (Exception ex){
+
+        }
+    }
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnxAgregarProducto:
+                agregarNuevosProductos("nuevo", JsonObject);
+                return true;
+
+            case R.id.mnxModificarProducto:
+                try {
+                    agregarNuevosProductos("modificar", datosJSON.getJSONObject(posicion));
+                }catch (Exception ex){}
+                return true;
+
+            case R.id.mnxEliminarProducto:
+
+                AlertDialog eliminarFriend =  eliminarProducto();
+                eliminarFriend.show();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
     private class obtenerDatostienda extends AsyncTask<Void,Void, String>{
         HttpURLConnection urLconection;
         @Override
@@ -84,22 +160,133 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private void mostrarDatosProductos(){
-        ListView ltsProductosCouchDB = findViewById(R.id.ltsProductosCouchDB);
+
+     //   ListView ltsProductosCouchDB = findViewById(R.id.ltsProductosCouchDB);
+   //     try {
+     //       final ArrayList<String>arrayList = new ArrayList<>();
+      //      final ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+  //          ltsProductosCouchDB.setAdapter(stringArrayAdapter);
+
+   //         for (int i = 0; i < datosJSON.length(); i++) {
+    //            stringArrayAdapter.add(datosJSON.getJSONObject(i).getJSONObject("value").getString("Nombre"));
+ //           }
+//            stringArrayAdapter.notifyDataSetChanged(); //reactualizar datos
+ //           registerForContextMenu(ltsProductosCouchDB);
+//        }catch (Exception ex){
+ //           Toast.makeText(MainActivity.this, "Error al mostrar los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+        ListView ltsProductos = findViewById(R.id.ltsProductosCouchDB);
         try {
-            final ArrayList<String>arrayList = new ArrayList<>();
-            final ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
-            ltsProductosCouchDB.setAdapter(stringArrayAdapter);
+            arrayList.clear();
+            stringArrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+            ltsProductos.setAdapter(stringArrayAdapter);
 
             for (int i = 0; i < datosJSON.length(); i++) {
                 stringArrayAdapter.add(datosJSON.getJSONObject(i).getJSONObject("value").getString("Nombre"));
             }
-            stringArrayAdapter.notifyDataSetChanged(); //reactualizar datos
-            registerForContextMenu(ltsProductosCouchDB);
+            copyStringArrayList.clear();
+            copyStringArrayList.addAll(arrayList);
+
+            stringArrayAdapter.notifyDataSetChanged();
+            registerForContextMenu(ltsProductos);
         }catch (Exception ex){
             Toast.makeText(MainActivity.this, "Error al mostrar los datos: " + ex.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
+    public void agregarNuevosProductos(String accion, JSONObject jsonObject){
+        try {
+            Bundle enviarParametros = new Bundle();
+            enviarParametros.putString("accion",accion);
+            enviarParametros.putString("dataProducto",jsonObject.toString());
+
+            Intent agregarProducto = new Intent(MainActivity.this, agregar_productos.class);
+            agregarProducto.putExtras(enviarParametros);
+            startActivity(agregarProducto);
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Error al llamar agregar producto: "+ e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+    AlertDialog eliminarProducto(){
+        AlertDialog.Builder confirmacion = new AlertDialog.Builder(MainActivity.this);
+        try {
+            confirmacion.setTitle(datosJSON.getJSONObject(posicion).getJSONObject("value").getString("Nombre"));
+            confirmacion.setMessage("Esta seguro de eliminar el registro?");
+            confirmacion.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    eliminarDatosProducto objEliminarProducto = new eliminarDatosProducto();
+                    objEliminarProducto.execute();
+
+                    Toast.makeText(getApplicationContext(), "Producto eliminado con exito.", Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
+                }
+            });
+            confirmacion.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(getApplicationContext(), "Eliminacion cancelada por el usuario.", Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
+                }
+            });
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Error al mostrar la confoirmacion: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return confirmacion.create();
+    }
+    private class eliminarDatosProducto extends AsyncTask<String,String, String> {
+        HttpURLConnection urlConnection;
+
+        @Override
+        protected String doInBackground(String... parametros) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String jsonResponse = null;
+            try {
+                URL url = new URL("Http://10.0.2.2:5984/db_tienda" +
+                        datosJSON.getJSONObject(posicion).getJSONObject("value").getString("_id") + "?rev=" +
+                        datosJSON.getJSONObject(posicion).getJSONObject("value").getString("_rev"));
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("DELETE");
+
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String inputLine;
+                StringBuffer stringBuffer = new StringBuffer();
+                while ((inputLine = reader.readLine()) != null) {
+                    stringBuffer.append(inputLine + "\n");
+                }
+                if (stringBuffer.length() == 0) {
+                    return null;
+                }
+                jsonResponse = stringBuffer.toString();
+                return jsonResponse;
+            } catch (Exception ex) {
+                //
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                if (jsonObject.getBoolean("ok")) {
+                    Toast.makeText(getApplicationContext(), "Datos de amigo guardado con exito", Toast.LENGTH_SHORT).show();
+                    obtenerDatostienda objObtenerProductos = new obtenerDatostienda();
+                    objObtenerProductos.execute();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error al intentar guardar datos de producto", Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Error al guardar producto: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
+
 
 
 
--- app/src/main/java/com/ugb/calculadora/agregar_productos.java
+++ app/src/main/java/com/ugb/calculadora/agregar_productos.java
@@ -2,13 +2,169 @@ package com.ugb.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class agregar_productos extends AppCompatActivity {
+    String resp, accion, id, rev;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_productos);
+        try {FloatingActionButton btnMostrarProductos = findViewById(R.id.btnMostrarProductos);
+            btnMostrarProductos.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    guardarAmigo();
+                }
+            });
+            mostrarDatosProducto();
+
+        }catch (Exception ex ){
+            Toast.makeText(getApplicationContext(), "Error al agregar amigos: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
     }
+
+}
+    void mostrarDatosProducto(){
+        try {
+            Bundle recibirParametros = getIntent().getExtras();
+            accion = recibirParametros.getString("accion");
+            if (accion.equals("modificar")){
+                JSONObject dataProducto = new JSONObject(recibirParametros.getString("dataProducto")).getJSONObject("value");
+
+                TextView tempVal = (TextView)findViewById(R.id.txtNombre);
+                tempVal.setText(dataProducto.getString("Nombre"));
+
+                tempVal = (TextView)findViewById(R.id.txtMarca);
+                tempVal.setText(dataProducto.getString("Marca"));
+
+                tempVal = (TextView)findViewById(R.id.txtCategoria);
+                tempVal.setText(dataProducto.getString("Categoria"));
+
+                tempVal = (TextView)findViewById(R.id.txtPrecio);
+                tempVal.setText(dataProducto.getString("Precio"));
+
+
+                id = dataProducto.getString("_id");
+                rev = dataProducto.getString("_rev");
+            }
+        }catch (Exception ex){
+            ///
+        }
+    }
+    private void mostrarProductos(){
+        Intent mostrarProductos = new Intent(agregar_productos.this, MainActivity.class);
+        startActivity(mostrarProductos);
+    }
+    private void guardarAmigo(){
+        TextView tempVal = findViewById(R.id.txtNombre);
+        String Nombre = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtMarca);
+        String Marca = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtCategoria);
+        String Categoria = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtPrecio);
+        String Precio = tempVal.getText().toString();
+
+
+        try {
+            JSONObject datosProducto = new JSONObject();
+            if (accion.equals("modificar")){
+                datosProducto.put("_id",id);
+                datosProducto.put("_rev",rev);
+            }
+            datosProducto.put("Nombre del producto", Nombre);
+            datosProducto.put("Marca", Marca);
+            datosProducto.put("Categoria", Categoria);
+            datosProducto.put("Precio", Precio);
+
+            enviarDatosProducto objGuardarProducto = new enviarDatosProducto();
+            objGuardarProducto.execute(datosProducto.toString());
+        }catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Error: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+    private class enviarDatosProducto extends AsyncTask<String,String, String> {
+        HttpURLConnection urlConnection;
+        @Override
+        protected String doInBackground(String... parametros) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String jsonResponse = null;
+            String jsonDatos = parametros[0];
+            BufferedReader reader;
+            try {
+                URL url = new URL("Http://10.0.2.2:5984/db_tienda");
+                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type","application/json");
+                urlConnection.setRequestProperty("Accept","application/json");
+
+                Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"));
+                writer.write(jsonDatos);
+                writer.close();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                if(inputStream==null){
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                resp = reader.toString();
+
+                String inputLine;
+                StringBuffer stringBuffer = new StringBuffer();
+                while ((inputLine=reader.readLine())!= null){
+                    stringBuffer.append(inputLine+"\n");
+                }
+                if(stringBuffer.length()==0){
+                    return null;
+                }
+                jsonResponse = stringBuffer.toString();
+                return jsonResponse;
+            }catch (Exception ex){
+                //
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try{
+                JSONObject jsonObject = new JSONObject(s);
+                if(jsonObject.getBoolean("ok")){
+                    Toast.makeText(getApplicationContext(), "Datos de producto guardado con exito", Toast.LENGTH_SHORT).show();
+                    mostrarProductos();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error al intentar guardar datos de producto", Toast.LENGTH_SHORT).show();
+                }
+            }catch (Exception e){
+                Toast.makeText(getApplicationContext(), "Error al guardar producto: "+e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
